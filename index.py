3320c231de2ce4d929b401fe643f59f08d1a93e0
Pablo <pablopelaia@gmail.com>
2020-05-26 18:08:13 -0300

    campos y cuadros de textos

--- index.py
+++ index.py
@@ -4,6 +4,7 @@ import sqlite3
 
 raiz= Tk()
 
+# Creo barra menu-----------------------------------------------------------------------
 barraMenu= Menu(raiz)
 raiz.config(menu=barraMenu, width=300, height=300)
 
@@ -27,5 +28,35 @@ barraMenu.add_cascade(label="BBDD", menu=dbMenu)
 barraMenu.add_cascade(label="Borrar", menu=borrarMenu)
 barraMenu.add_cascade(label="CRUD", men=menuCRUD)
 barraMenu.add_cascade(label="Ayuda", menu=ayudaMenu)
+
+# Comienzo de campos-----------------------------------------------------------------------
+
+miFrame= Frame(raiz)
+miFrame.pack()
+
+cuadroID= Entry(miFrame)
+cuadroID.grid(row=0, column=1, padx=10, pady=10)
+
+cuadroNombre= Entry(miFrame)
+cuadroNombre.grid(row=1, column=1, padx=10, pady=10)
+cuadroNombre.config(fg="red", justify="right")
+
+cuadroPass= Entry(miFrame)
+cuadroPass.grid(row=2, column=1, padx=10, pady=10)
+cuadroPass.config(fg="red", justify="right", show="?")
+
+cuadroApellido= Entry(miFrame)
+cuadroApellido.grid(row=3, column=1, padx=10, pady=10)
+
+cuadroAdress= Entry(miFrame)
+cuadroAdress.grid(row=4, column=1, padx=10, pady=10)
+
+comentarios= Text(miFrame, width=16, height=5)
+comentarios.grid(row=5, column=1, padx=10, pady=10)
+
+scrVertical=Scrollbar(miFrame, command=comentarios.yview)
+scrVertical.grid(row=5, column=2, sticky="nsew")
+
+comentarios.config(yscrollcommand=scrVertical.set)
 
 raiz.mainloop()
