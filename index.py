7f0afd8e3b538ab3ac642d4721a8fa18d5550250
Pablo <pablopelaia@gmail.com>
2020-05-26 19:07:54 -0300

    conectando db

--- index.py
+++ index.py
@@ -2,14 +2,44 @@ from tkinter import *
 from tkinter import messagebox
 import sqlite3
 
+# Funciones----------------------------------------------------------------------------
+
+def conexionBBDD():
+
+    cnx= sqlite3.connect("Usuario")
+
+    miCursor= cnx.cursor()
+
+    try:
+
+        miCursor.execute('''CREATE TABLE DATO_USUARIOS (
+                ID INTEGER PRIMARY KEY AUTOINCREMENT,
+                NOMBRE_USUARIO VARCHAR(50),
+                CONTRASENA VARCHAR(50),
+                APELLIDO VARCHAR(10),
+                DIRECCION VARCHAR(50),
+                COMENTARIOS VARCHAR(100) 
+            )
+            ''')
+        
+        messagebox.showinfo("BBDD", "Base de datos creada")
+    
+    except:
+        
+        messagebox.showwarning("¡Atención!", "La base de datos ya existe")
+ 
+
+
+
 raiz= Tk()
 
 # Creo barra menu-----------------------------------------------------------------------
+
 barraMenu= Menu(raiz)
 raiz.config(menu=barraMenu, width=300, height=300)
 
 dbMenu=Menu(barraMenu, tearoff=0)
-dbMenu.add_command(label="Conectar")
+dbMenu.add_command(label="Conectar", command=conexionBBDD)
 dbMenu.add_command(label="Salir")
 
 borrarMenu=Menu(barraMenu, tearoff=0)
@@ -58,5 +88,43 @@ scrVertical=Scrollbar(miFrame, command=comentarios.yview)
 scrVertical.grid(row=5, column=2, sticky="nsew")
 
 comentarios.config(yscrollcommand=scrVertical.set)
+
+# Etiquetas-----------------------------------------------------------------------
+
+idLabel= Label(miFrame, text="ID:")
+idLabel.grid(row=0, column=0, sticky="e", padx=10, pady=10)
+
+nombreLabel= Label(miFrame, text="Nombre:")
+nombreLabel.grid(row=1, column=0, sticky="e", padx=10, pady=10)
+
+apellidoLabel= Label(miFrame, text="Apellido:")
+apellidoLabel.grid(row=2, column=0, sticky="e", padx=10, pady=10)
+
+passLabel= Label(miFrame, text="Contraseña:")
+passLabel.grid(row=3, column=0, sticky="e", padx=10, pady=10)
+
+adressLabel= Label(miFrame, text="Dirección:")
+adressLabel.grid(row=4, column=0, sticky="e", padx=10, pady=10)
+
+comentariosLabel= Label(miFrame, text="Comentarios:")
+comentariosLabel.grid(row=5, column=0, sticky="e", padx=10, pady=10)
+
+# Botones-----------------------------------------------------------------------
+
+botonera= Frame(raiz)
+botonera.pack()
+
+btnCrear= Button(botonera, text="Crear")
+btnCrear.grid(row=0, column=0, sticky="e", padx=10, pady=10)
+
+btnLeer= Button(botonera, text="Leer")
+btnLeer.grid(row=0, column=1, sticky="e", padx=10, pady=10)
+
+btnActualizar= Button(botonera, text="Actualizar")
+btnActualizar.grid(row=0, column=2, sticky="e", padx=10, pady=10)
+
+btnBorrar= Button(botonera, text="Borrar")
+btnBorrar.grid(row=0, column=3, sticky="e", padx=10, pady=10)
+
 
 raiz.mainloop()
